48befdab9e4a1c777a2c572457e38783c40e5c82
Permitin Yury <ypermitin@yandex.ru>
2018-10-18 12:18:27 +0500

    Code cleanup

--- 1С-Extended-Database-Settings-Maintenance/Tables/dbo.EventType.sql
+++ 1С-Extended-Database-Settings-Maintenance/Tables/dbo.EventType.sql
@@ -1,8 +1,9 @@
-﻿-- =============================================================================================================
+/* =============================================================================================================
 -- Author:		Permitin Y.A. (ypermitin@yandex.ru)
 -- Create date: 2018-10-15
 -- Description:	Таблица для хранения списка возможных типов событий записей логов
--- =============================================================================================================
+-- =============================================================================================================*/
+
 CREATE TABLE [dbo].[EventType] (
   [ID] [int] NOT NULL,
   [Name] [nvarchar](250) NOT NULL,
@@ -11,17 +12,11 @@ CREATE TABLE [dbo].[EventType] (
 ON [PRIMARY]
 GO
 
-SET IDENTITY_INSERT [dbo].[EventType] ON 
-GO
 INSERT [dbo].[EventType] ([ID], [Name]) VALUES (4, N'On Table Create')
 GO
 INSERT [dbo].[EventType] ([ID], [Name]) VALUES (5, N'On Index Create')
 GO
-SET IDENTITY_INSERT [dbo].[EventType] OFF
-GO
 
-SET QUOTED_IDENTIFIER, ANSI_NULLS ON
-GO
 CREATE TRIGGER [EventType_Block_Cnanges]
 ON [dbo].[EventType]
 FOR INSERT, UPDATE
