0f66c3cbf8c564d716c7298fd33d993bfa3fade0
Permitin Yury <ypermitin@yandex.ru>
2018-10-18 12:05:55 +0500

    Code cleanup

--- 1С-Extended-Database-Settings-Maintenance/Programmability/Procedures/dbo.LogError.sql
+++ 1С-Extended-Database-Settings-Maintenance/Programmability/Procedures/dbo.LogError.sql
@@ -1,6 +1,3 @@
-﻿SET QUOTED_IDENTIFIER, ANSI_NULLS ON
-GO
-
 -- =============================================================================================================
 -- Author:		Permitin Y.A. (ypermitin@yandex.ru)
 -- Create date: 2018-10-15
