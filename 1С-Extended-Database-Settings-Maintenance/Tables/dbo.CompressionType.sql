dbcc1d4bc65c88413c412adb5e59b46981326ebc
Permitin Yury <ypermitin@yandex.ru>
2018-10-18 12:19:09 +0500

    Code cleanup

--- 1С-Extended-Database-Settings-Maintenance/Tables/dbo.CompressionType.sql
+++ 1С-Extended-Database-Settings-Maintenance/Tables/dbo.CompressionType.sql
@@ -1,8 +1,8 @@
-﻿-- =============================================================================================================
+/* =============================================================================================================
 -- Author:		Permitin Y.A. (ypermitin@yandex.ru)
 -- Create date: 2018-10-15
 -- Description:	Таблица для хранения списка доступных методов сжатия объектов баз данных
--- =============================================================================================================
+-- =============================================================================================================*/
 CREATE TABLE [dbo].[CompressionType] (
   [ID] [int] NOT NULL,
   [Name] [nvarchar](150) NOT NULL,
@@ -11,18 +11,12 @@ CREATE TABLE [dbo].[CompressionType] (
 ON [PRIMARY]
 GO
 
-SET QUOTED_IDENTIFIER, ANSI_NULLS ON
-GO
-
-SET IDENTITY_INSERT [dbo].[CompressionType] ON 
-GO
 INSERT [dbo].[CompressionType] ([ID], [Name]) VALUES (1, N'None')
 GO
 INSERT [dbo].[CompressionType] ([ID], [Name]) VALUES (2, N'Row')
 GO
 INSERT [dbo].[CompressionType] ([ID], [Name]) VALUES (3, N'Page')
 GO
-SET IDENTITY_INSERT [dbo].[CompressionType] OFF
 
 CREATE TRIGGER [CompressionType_Block_Cnanges]
 ON [dbo].[CompressionType]
