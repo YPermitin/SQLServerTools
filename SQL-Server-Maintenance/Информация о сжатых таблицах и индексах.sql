14d1b06c95c129af1b24d4ab159c03fdb4a7f2b1
Permitin Yury <ypermitin@yandex.ru>
2019-02-06 00:28:16 +0500

    Исправил отображение имени индекса
    если сжатие используется для него.

--- SQL-Server-Maintenance/Информация о сжатых таблицах и индексах.sql
+++ SQL-Server-Maintenance/Информация о сжатых таблицах и индексах.sql
@@ -1,11 +1,14 @@
 SELECT 
 	st.name AS [Имя таблицы], 
-	OBJECT_NAME(st.object_id) AS [Имя объекта], 
+	ISNULL(INDX.name, st.name) AS [Имя объекта], 
 	sp.partition_id AS [Идентификатор секции], 
 	sp.partition_number AS [Номер секции], 
 	sp.data_compression AS [Тип сжатия], 
-	sp.data_compression_desc AS [Имя типа сжатия]
+	sp.data_compression_desc AS [Имя типа сжатия]	
 FROM sys.partitions SP
-	INNER JOIN sys.tables ST 
-	ON st.object_id = sp.object_id
+	INNER JOIN sys.tables ST	
+		ON st.object_id = sp.object_id
+	LEFT JOIN sys.indexes INDX
+		ON INDX.object_id = ST.object_id
+			AND INDX.index_id = SP.index_id
 WHERE data_compression <> 0
