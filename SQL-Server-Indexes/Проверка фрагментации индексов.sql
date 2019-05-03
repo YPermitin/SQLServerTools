9778d662376865e0494b264000d561aa3e94651e
Permitin Yury <ypermitin@yandex.ru>
2019-05-04 01:54:21 +0500

    Обновил скрипт проверки фрагментации индексов

--- SQL-Server-Indexes/Проверка фрагментации индексов.sql
+++ SQL-Server-Indexes/Проверка фрагментации индексов.sql
@@ -1,2 +1,8 @@
-select OBJECT_NAME(object_id) name, * from sys.dm_db_index_physical_stats(DB_ID(), null, null, null, null)
-where avg_fragmentation_in_percent > 30
+SELECT
+	DB_NAME([IF].database_id) AS [Имя базы] 
+	,OBJECT_NAME(object_id) AS [Имя таблицы]	
+	,OBJECT_NAME([IF].index_id) AS [Имя индкса]	
+	,[IF].*
+FROM sys.dm_db_index_physical_stats(DB_ID(), null, null, null, null) AS [IF]
+WHERE avg_fragmentation_in_percent > 30
+ORDER BY avg_fragmentation_in_percent
