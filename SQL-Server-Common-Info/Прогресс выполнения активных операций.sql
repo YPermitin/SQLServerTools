6ce2409879a10d7f4af1086c829bf29c4ad08baf
YPermitin <ypermitin@yandex.ru>
2019-10-18 21:05:01 +0500

    Добавлен скрипт просмотра прогресса выполнения активных операций

---
+++ SQL-Server-Common-Info/Прогресс выполнения активных операций.sql
@@ -0,0 +1,14 @@
+SELECT 
+    @@Servername AS [Server],
+    DB_NAME(database_id) [DatabaseName],
+    command [Command],
+    percent_complete [PercentComplete],
+    session_id [ConnectionId],
+    start_time [StartTime],
+    status [Status],
+    txtsql.text [QueryText],
+    USER_NAME(user_id) [User],
+    wait_type [WaitType]
+FROM sys.dm_exec_requests der
+    CROSS APPLY sys.dm_exec_sql_text(t.sql_handle) txtsql
+WHERE ISNULL(percent_complete,0) > 0
