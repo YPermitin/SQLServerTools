6dc40e28fb28c033f58d0ea4ef33247891c37f87
YPermitin <ypermitin@yandex.ru>
2020-10-17 23:41:10 +0500

    Исправлены и дополнены скрипты обслуживания

--- SQL-Server-Maintenance/Очистка процедурного кэша.sql
+++ SQL-Server-Maintenance/Очистка процедурного кэша.sql
@@ -1,1 +1,4 @@
+-- Подробная информация: https://docs.microsoft.com/ru-ru/sql/t-sql/database-console-commands/dbcc-freeproccache-transact-sql?view=sql-server-ver15
+-- Используйте эту комманду аккуратно.
+
 DBCC FREEPROCCACHE
