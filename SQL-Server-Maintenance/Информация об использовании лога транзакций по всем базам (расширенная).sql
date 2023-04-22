26d1f83ab1e1a72ed1d3e5f5f1d0f48035ed506b
YPermitin <ypermitin@yandex.ru>
2023-04-22 10:32:49 +0500

    Расширена аналитика в скрипте анализя логов транзакций баз данных

--- SQL-Server-Maintenance/Информация об использовании лога транзакций по всем базам (расширенная).sql
+++ SQL-Server-Maintenance/Информация об использовании лога транзакций по всем базам (расширенная).sql
@@ -13,13 +13,13 @@
 * Размер свободного пространства в файле лога
 
 Пример:
-| DatabaseName        | LogFileName             | LogFilePath                               | Disk | DiskFreeSpaceMB | LogSizeMB | LogMaxSizeMB | LogFileCanGrow | LogFileFreeSpaceMB |
-|---------------------|-------------------------|-------------------------------------------|------|-----------------|-----------|--------------|----------------|--------------------|
-| master              | mastlog                 | F:\SQLLOGS\mastlog.ldf                    | F:\  | 1000000         | 1000      | 0            | 1              | 928                |
-| tempdb              | templog                 | F:\SQLLOGS\templog.ldf                    | F:\  | 1000000         | 100000    | 400000       | 1              | 99999              |
-| model               | modellog                | F:\SQLLOGS\modellog.ldf                   | F:\  | 1000000         | 8         | 0            | 1              | 7                  |
-| msdb                | MSDBLog                 | F:\SQLLOGS\MSDBLog.ldf                    | F:\  | 1000000         | 300       | 2097152      | 1              | 290                |
-| SQLServerMonitoring | SQLServerMonitoring_log | F:\SQLLOGS\SQLServerMonitoringLog.ldf     | F:\  | 1000000         | 1000      | 2097152      | 1              | 999                |
+| DatabaseName        | LogFileName             | LogFilePath                               | Disk | DiskFreeSpaceMB | LogSizeMB | LogMaxSizeMB | LogFileCanGrow | LogFileFreeSpaceMB | LogFileUsedPercent | TotalLogMaxSizeMB | TotalLogFileFreeMB | TotalLogFileUsedPercent |
+|---------------------|-------------------------|-------------------------------------------|------|-----------------|-----------|--------------|----------------|--------------------|--------------------|-------------------|--------------------|-------------------------|
+| master              | mastlog                 | F:\SQLLOGS\mastlog.ldf                    | F:\  | 1000000         | 1000      | 2097152      | 1              | 928                | 7                  | 2097152           | 2097152            | 0                       |
+| tempdb              | templog                 | F:\SQLLOGS\templog.ldf                    | F:\  | 1000000         | 100000    | 400000       | 1              | 99999              | 0                  | 400000            | 300001             | 75                      |
+| model               | modellog                | F:\SQLLOGS\modellog.ldf                   | F:\  | 1000000         | 8         | 2097152      | 1              | 7                  | 1                  | 2097152           | 2097151            | 0                       |
+| msdb                | MSDBLog                 | F:\SQLLOGS\MSDBLog.ldf                    | F:\  | 1000000         | 300       | 2097152      | 1              | 290                | 3                  | 2097152           | 2097152            | 0                       |
+| SQLServerMonitoring | SQLServerMonitoring_log | F:\SQLLOGS\SQLServerMonitoringLog.ldf     | F:\  | 1000000         | 1000      | 2097152      | 1              | 999                | 0                  | 2097152           | 2097151            | 0                       |
 */
 
 IF OBJECT_ID('tempdb..#logFileInfoByDatabases') IS NOT NULL
@@ -34,7 +34,8 @@ CREATE TABLE #logFileInfoByDatabases
 	[LogSizeMB] numeric(15,0),
 	[LogMaxSizeMB] numeric(15,0),
 	[LogFileCanGrow] bit,
-	[LogFileFreeSpaceMB] numeric(15,0)
+	[LogFileFreeSpaceMB] numeric(15,0),
+	[LogFileUsedPercent] numeric(15,0)
 );
 
 DECLARE
@@ -52,23 +53,41 @@ BEGIN
 		+ CHAR(13)+ CHAR(10)
 		+ N'INSERT INTO #logFileInfoByDatabases
 SELECT
-	DB_NAME(f.database_id) AS [Database],
-	f.[name] AS [LogFileName],
-	f.physical_name AS [LogFilePath],
-	volume_mount_point AS [Disk],
-	available_bytes/1048576 as [DiskFreeSpaceMB],
-	CAST(f.size AS bigint) * 8 / 1024 AS [LogSizeMB],
-	CAST(f.max_size AS bigint) * 8 / 1024 AS [LogMaxSizeMB],
+	dt.[Database],
+	dt.[LogFileName],
+	dt.[LogFilePath],
+	dt.[Disk],
+	dt.[DiskFreeSpaceMB],
+	dt.[LogSizeMB],
+	dt.[LogMaxSizeMB],
+	dt.[LogFileCanGrow],
+	dt.[LogFileFreeSpaceMB],
 	CASE 
-		WHEN (CAST(f.size AS bigint) * 8 / 1024) = (CAST(CASE WHEN f.max_size = 0 THEN 268435456 ELSE f.max_size END AS bigint) * 8 / 1024)
+		WHEN LogSizeMB / CAST(100.0 AS DECIMAL(25,2)) = 0
 		THEN 0
-		ELSE 1
-	END AS [LogFileCanGrow],
-	size/128.0 - CAST(FILEPROPERTY(f.[name],''SpaceUsed'') AS INT)/128.0 AS [LogFileFreeSpaceMB]
-FROM sys.master_files AS f CROSS APPLY 
-  sys.dm_os_volume_stats(f.database_id, f.file_id)
-WHERE [type_desc] = ''LOG''
-	and f.database_id = DB_ID();';
+		ELSE 100 - (LogFileFreeSpaceMB / (LogSizeMB / CAST(100.0 AS DECIMAL(25,2))))
+	END AS LogFileUsedPercent
+FROM (
+		SELECT
+			DB_NAME(f.database_id) AS [Database],
+			f.[name] AS [LogFileName],
+			f.physical_name AS [LogFilePath],
+			volume_mount_point AS [Disk],
+			available_bytes/1048576 as [DiskFreeSpaceMB],
+			CAST(f.size AS bigint) * 8 / 1024 AS [LogSizeMB],
+			CAST(CASE WHEN f.max_size <= 0 THEN 268435456 ELSE f.max_size END AS bigint) * 8 / 1024 AS [LogMaxSizeMB],
+			CASE 
+				WHEN (CAST(f.size AS bigint) * 8 / 1024) = (CAST(CASE WHEN f.max_size = 0 THEN 268435456 ELSE f.max_size END AS bigint) * 8 / 1024)
+				THEN 0
+				ELSE 1
+			END AS [LogFileCanGrow],
+			size/128.0 - CAST(FILEPROPERTY(f.[name],''SpaceUsed'') AS INT)/128.0 AS [LogFileFreeSpaceMB]
+		FROM sys.master_files AS f CROSS APPLY 
+		  sys.dm_os_volume_stats(f.database_id, f.file_id)
+		WHERE [type_desc] = ''LOG''
+			and f.database_id = DB_ID()
+	) dt
+';
 
 	EXECUTE(@SqlStatement);
 	
@@ -77,8 +96,45 @@ CLOSE DatabaseList;
 DEALLOCATE DatabaseList;
 
 SELECT
-	*
-FROM #logFileInfoByDatabases
+	-- Имя базы данных
+	lf.[DatabaseName],
+	-- Имя файла лога транзакций
+	lf.[LogFileName],
+	-- Путь к файлу лога транзакций
+	lf.[LogFilePath],
+	-- Имя диска, где находится файл лога
+	lf.[Disk],
+	-- Свободное место на диске с файлом лога
+	lf.[DiskFreeSpaceMB],
+	-- Размер файла лога транзакций
+	lf.[LogSizeMB],
+	-- Максимально возможный размер файла лога транзакций (с учетом ограничений настроек роста файла лога)
+	lf.[LogMaxSizeMB],
+	-- Файл лога транзакций может расти (с учетом ограничений настроек роста файла лога)
+	lf.[LogFIleCanGrow],
+	-- Свободное место в файле лога транзакций
+	lf.[LogFileFreeSpaceMB],
+	-- Процент использования файла лога транзакций
+	lf.[LogFileUsedPercent],
+	-- Максимальный размер лога транзакций для базы данных с учетом всех файлов логов и их настроек (общий для базы) 
+	totals.[TotalLogMaxSizeMB],
+	-- Свободно пространства для лога транзакций для базы данных с учетом всех файлов логов и их настроек (общий для базы)
+	totals.[TotalLogFileFreeMB],
+	-- Процент использования лога транзакций у четом общего макс. размера для базы данных с учетом всех файлов логов и их настроек (общий для базы)
+	CASE
+		WHEN ([TotalLogMaxSizeMB] / 100) = 0
+		THEN 0
+		ELSE 100 - [TotalLogFileFreeMB] / ([TotalLogMaxSizeMB] / 100)
+	END AS [TotalLogFileUsedPercent]
+FROM #logFileInfoByDatabases lf
+	LEFT JOIN (
+		SELECT
+			DatabaseName,
+			SUM(LogMaxSizeMB) AS [TotalLogMaxSizeMB],
+			SUM(LogMaxSizeMB - (LogSizeMB - LogFileFreeSpaceMB)) AS [TotalLogFileFreeMB]
+		FROM #logFileInfoByDatabases
+		GROUP BY DatabaseName
+	) totals ON lf.DatabaseName = totals.DatabaseName
 
 IF OBJECT_ID('tempdb..#logFileInfoByDatabases') IS NOT NULL
 	DROP TABLE #logFileInfoByDatabases;
